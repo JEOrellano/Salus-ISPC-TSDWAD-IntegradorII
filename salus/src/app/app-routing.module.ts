0d2870a3ebdefd481c4e47e3e29ea224301ea860
Angelica Orozco <angorozcar@gmail.com>
2023-05-01 16:49:31 -0300

    info and styles

--- salus/src/app/app-routing.module.ts
+++ salus/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { RegistroComponent } from './pages/registro/registro.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path:'nosotros', component:NosotrosComponent},
+  {path:'contacto', component:ContactoComponent},
+  {path:'registro', component:RegistroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
