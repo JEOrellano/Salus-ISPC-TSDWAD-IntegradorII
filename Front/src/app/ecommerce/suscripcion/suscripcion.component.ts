1c9e0d960109cac393bb7f4f28c209559518fa5f
Angelica Orozco <angorozcar@gmail.com>
2023-05-17 20:38:10 -0300

    consumiendo la API de suscripciones

--- Front/src/app/ecommerce/suscripcion/suscripcion.component.ts
+++ Front/src/app/ecommerce/suscripcion/suscripcion.component.ts
@@ -4,12 +4,41 @@ import { Suscription } from 'src/app/model/suscription.model';
 
 @Component({
   selector: 'app-suscripcion',
-  templateUrl: './suscripcion.component.html',
+  //templateUrl: './suscripcion.component.html',
+  template: `
+    <div class="container mb-5 mt-2 " >
+    <div class="row">
+      <div class="col-lg-4" *ngFor="let dato of mySuscriptions">
+        <div class="mb-5 mb-lg-0">
+          <div class="card-body suscription-register">
+            <h3 class="card-title text-muted text-uppercase text-center">
+              {{dato.tipoServicio}}
+            </h3>
+            <h5 class="card-price text-center">
+              {{dato.precio}}<span class="period">/mes</span>
+            </h5>
+            <hr />
+            <ul class="fa-ul">
+              <li *ngFor="let item of dato.descripcion">
+                <span class="fa-li"><i class="fas fa-check"></i></span>{{item}}
+              </li>
+            </ul>
+            <div class="d-grid">
+              <a href="#" class="btn botons text-uppercase">Suscríbete</a>
+            </div>
+          </div>
+        </div>
+      </div>    
+    </div>
+  </div>
+  `,
   styleUrls: ['./suscripcion.component.css']
 })
 export class SuscripcionComponent implements OnInit{
 
   mySuscriptions: Suscription[] = [];
+ myServices: Suscription["descripcion"] = [];
+  
 
   choseSuscription: {} = {};
 
@@ -20,6 +49,7 @@ export class SuscripcionComponent implements OnInit{
     this.suscriptionService.getAllSuscriptions()
     .subscribe(data => {
       this.mySuscriptions = data
+      console.log(this.mySuscriptions)
     })
   }
 
@@ -36,6 +66,6 @@ export class SuscripcionComponent implements OnInit{
       this.choseSuscription = data;
       console.log(this.choseSuscription)
     })
-
   }
+  
 }
